4983a1623d64e1c787bc4926818f57651e35bb00
Tamal2401 <onlyjohn98@gmail.com>
2020-03-16 02:51:32 +0530

    Spring MongoDB project

---
+++ Spring Mongo/src/main/java/com/webscraper/java/Spring/Mongo/SpringMongoApplication.java
@@ -0,0 +1,69 @@
+package com.webscraper.java.Spring.Mongo;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootApplication
+public class SpringMongoApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMongoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        String mainUrl = "https://www.naukri.com/deloitte-jobs-in-hyderabad";
+        // hit the job url
+        Document doc = Jsoup.connect(mainUrl).get();
+
+        // initializing main jobdetails list
+        List<Element> jobsList = new ArrayList<>();
+
+
+        // check if next page exists
+        boolean isNext = true;
+        while (isNext) {
+            // get job details from the page and populate the domain object
+            List<Element> tempList = doc.getElementsByAttributeValue("id", "jdUrl");
+            jobsList.addAll(tempList);
+
+            isNext = checkNextPage(doc);
+            if (!isNext) {
+                break;
+            }
+            Element nextPageElement = doc.getElementsByAttributeValue("class", "pagination").get(0);
+            String nextPageUrl = "";
+            List<Node> childNodes = nextPageElement.childNodes();
+            Optional<Node> nextButtonNode = childNodes.stream()
+                    .filter(node -> node.toString().toLowerCase().contains("next"))
+                    .findFirst();
+            if (nextButtonNode.isPresent()) {
+                Node childNode = nextButtonNode.get();
+                nextPageUrl = childNode.attr("href");
+            }
+            doc = Jsoup.connect(nextPageUrl).get();
+        }
+		System.out.println(jobsList.size());
+		jobsList.forEach(each-> System.out.println(each));
+    }
+
+    private boolean checkNextPage(Document doc) {
+        Elements buttonElement = doc.getElementsByAttributeValue("class", "pagination");
+        String text = buttonElement.text().toLowerCase();
+        if (text.contains("next")) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Spring Mongo/src/test/java/com/webscraper/java/Spring/Mongo/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webscraper.java.Spring.Mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
