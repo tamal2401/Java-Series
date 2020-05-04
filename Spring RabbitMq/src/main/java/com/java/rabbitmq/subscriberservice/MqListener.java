c7fa82452ea66044ab8977a5d874ff169c6f3ca0
Tamal2401 <onlyjohn98@gmail.com>
2020-05-04 21:53:03 +0530

    MessegeListner changes added

--- Spring RabbitMq/src/main/java/com/java/rabbitmq/subscriberservice/MqListener.java
+++ Spring RabbitMq/src/main/java/com/java/rabbitmq/subscriberservice/MqListener.java
@@ -1,9 +1,41 @@
 package com.java.rabbitmq.subscriberservice;
 
+import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MqListener {
 
+    @Autowired
+    Queue queue;
 
+    public SimpleMessageListenerContainer container(ConnectionFactory connectionFactory){
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueues(queue);
+        container.setMessageListener(listener());
+        return  container;
+    }
+
+    public MessageListener listener(){
+        MessageListener listener = (msg)->{
+            System.out.println("Recived Messege is :: ".concat(new String(msg.getBody())));
+        };
+        return listener;
+    }
+
+    //create custom connection factory
+	/*@Bean
+	ConnectionFactory connectionFactory() {
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
+		cachingConnectionFactory.setUsername(username);
+		cachingConnectionFactory.setUsername(password);
+		return cachingConnectionFactory;
+	}*/
 }
