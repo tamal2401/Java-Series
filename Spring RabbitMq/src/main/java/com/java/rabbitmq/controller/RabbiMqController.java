d2a2319d87d9246fc516a94ea56337a280727ec6
Tamal2401 <onlyjohn98@gmail.com>
2020-05-05 23:20:58 +0530

    config changes
    mq host name, ip name

--- Spring RabbitMq/src/main/java/com/java/rabbitmq/controller/RabbiMqController.java
+++ Spring RabbitMq/src/main/java/com/java/rabbitmq/controller/RabbiMqController.java
@@ -1,6 +1,8 @@
 package com.java.rabbitmq.controller;
 
 import com.java.rabbitmq.domainobject.Book;
+import com.java.rabbitmq.producerservice.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RabbiMqController {
 
+    @Autowired
+    ProducerService template;
+
     @PostMapping(value="/post/message", produces = "application/json")
     public String sendMessage(@RequestBody Book book){
-        return "";
+        try{
+            template.sendMessage(book);
+        }catch(Exception e){
+            System.out.println("Exception while queing messege :: "+e.getMessage());
+        }
+        return "Messege sent";
     }
 }
