2f85c026c9d9c98552625cec5facb2ac65447380
Tamal2401 <onlyjohn98@gmail.com>
2020-06-07 19:06:30 +0530

    rxJava sample practice code added

--- Java-Custom-DS/src/main/java/com/java/ds/concurrency/reactivejava/BasicObserverExample.java
+++ Java-Custom-DS/src/main/java/com/java/ds/concurrency/reactivejava/BasicObserverExample.java
@@ -1,10 +1,78 @@
 package com.java.ds.concurrency.reactivejava;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class BasicObserverExample {
 
     public static void main(String[] args) {
-        Observable<String> observable = Observable.just("basic rxJava example");
+        Observable<Stock> observable = getObserver();
+
+        observable.filter(each -> each.getStockname().equalsIgnoreCase("google"))
+                .map(each -> {
+                    each.setStockValue(each.getStockValue()+1234);
+                    return each;
+                })
+                .subscribe(stock -> System.out.println(stock.toString()));
+    }
+
+    private static Observable<Stock> getObserver() {
+        List<String> listOfCompanies = Arrays.asList("google", "facebook", "amazon");
+        return Observable.create(emitter -> getStockValues(emitter, listOfCompanies));
+    }
+
+    private static void getStockValues(ObservableEmitter<Stock> emitter, List<String> listOfCompanies) {
+        while (true) {
+            listOfCompanies.stream()
+                    .map(each -> new Stock(each, 1234))
+                    .forEach(emitter::onNext);
+            sleep(2000);
+        }
+    }
+
+    private static void sleep(long i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            System.out.println("error occured");
+        }
+    }
+}
+
+class Stock {
+    private String stockname;
+    private int stockValue;
+
+    public Stock(String stockname, int stockValue) {
+        this.stockname = stockname;
+        this.stockValue = stockValue;
+    }
+
+    public String getStockname() {
+        return stockname;
+    }
+
+    public void setStockname(String stockname) {
+        this.stockname = stockname;
+    }
+
+    public int getStockValue() {
+        return stockValue;
+    }
+
+    public void setStockValue(int stockValue) {
+        this.stockValue = stockValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "stockname='" + stockname + '\'' +
+                ", stockValue=" + stockValue +
+                '}';
     }
 }
