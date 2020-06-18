3c8c7af22356c943ba9e2ddeb25e9200bd0ecc80
Tamal2401 <onlyjohn98@gmail.com>
2020-06-18 23:57:52 +0530

    user added

--- Spring-Security-JDBC/src/main/java/com/spring/security/springsecurity/authentication/CustomAuthenticaionManager.java
+++ Spring-Security-JDBC/src/main/java/com/spring/security/springsecurity/authentication/CustomAuthenticaionManager.java
@@ -1,19 +1,38 @@
 package com.spring.security.springsecurity.authentication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class CustomAuthenticaionManager extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource source;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth.jdbcAuthentication()
+                .withDefaultSchema()
+                .dataSource(source)
+                .withUser(
+                        User.withUsername("tamal")
+                            .password("test")
+                            .roles("admin")
+                )
+                .withUser(
+                        User.withUsername("user")
+                                .password("test")
+                                .roles("user")
+                );
     }
 
     @Override
@@ -24,5 +43,10 @@ public class CustomAuthenticaionManager extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .and()
                 .formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
