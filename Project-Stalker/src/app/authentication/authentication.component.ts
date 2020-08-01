108e466ac47f0de76ed48914ba8b7b5a8af5d1ce
Tamal2401 <onlyjohn98@gmail.com>
2020-08-02 01:06:45 +0530

    Update authentication.component.ts

--- Project-Stalker/src/app/authentication/authentication.component.ts
+++ Project-Stalker/src/app/authentication/authentication.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from '../model/User';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../services/auth-service';
 
 @Component({
   selector: 'app-authentication',
@@ -14,14 +15,17 @@ export class AuthenticationComponent implements OnInit {
   user : User = null;
   @ViewChild('authForm', { static: false }) form: NgForm;
 
-  constructor(private route:Router,
-              private aRoute: ActivatedRoute) { }
+  constructor(private route : Router,
+              private authService : AuthService) { }
 
   ngOnInit(): void {
     console.log(this.isLoginMode);
   }
 
   submitForm() {
+    if(!this.form.valid){
+      return;
+    }
     this.user = new User(this.form.control.value.userName, this.form.control.value.pwd);
     console.log(this.user);
     this.route.navigate(['profile']);
