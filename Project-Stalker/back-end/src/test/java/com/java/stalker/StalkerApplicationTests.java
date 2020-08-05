c8ee318524997e066c9d503784def3d6fce6a88b
Tamal2401 <das.tamal00496@gmail.com>
2020-08-06 02:30:43 +0530

    java project added

---
+++ Project-Stalker/back-end/src/test/java/com/java/stalker/StalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.stalker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StalkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
