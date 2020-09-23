b374b6c52afc45ace9dd14aa893b4c9752fd1bf9
Tamal2401 <das.tamal00496@gmail.com>
2020-09-23 23:35:04 +0530

    okta project added

---
+++ Spring-OktaConnect/src/main/java/com/okta/openid/OktaConnect/OktaConnectApplication.java
@@ -0,0 +1,13 @@
+package com.okta.openid.OktaConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaConnectApplication.class, args);
+	}
+
+}
---
+++ Spring-OktaConnect/src/test/java/com/okta/openid/OktaConnect/OktaConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.openid.OktaConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktaConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
