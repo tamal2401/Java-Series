661b96e921a1527d1080d84f6abb2199a22d7cb1
Tamal2401 <das.tamal00496@gmail.com>
2020-09-24 03:14:04 +0530

    okta authentication integratiom is succssfull

--- Spring-OktaConnect/src/main/java/com/okta/openid/OktaConnect/controller/HomeController.java
+++ Spring-OktaConnect/src/main/java/com/okta/openid/OktaConnect/controller/HomeController.java
@@ -1,15 +1,46 @@
 package com.okta.openid.OktaConnect.controller;
 
+import com.okta.sdk.client.Client;
+import com.okta.sdk.resource.user.User;
+import com.okta.sdk.resource.user.UserBuilder;
+import com.okta.sdk.resource.user.UserList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Autowired
+    public Client client;
+
     @GetMapping("/")
     public String home(@AuthenticationPrincipal OidcUser user) {
         return "Welcome, " + user.getFullName() + "!";
+    }
+
+    @GetMapping("/users")
+    public UserList getUsers() {
+        return client.listUsers();
+    }
+
+    @GetMapping("/user")
+    public UserList searchUserByEmail(@RequestParam String query) {
+        return client.listUsers(query, null, null, null, null);
+    }
+
+    @GetMapping("/createUser")
+    public User createUser() {
+        User user = UserBuilder.instance()
+                .setEmail("normal.lewis@email.com")
+                .setFirstName("Norman")
+                .setLastName("Lewis")
+                .setPassword("tempPassword".toCharArray())
+                .setActive(true)
+                .buildAndCreate(client);
+        return user;
     }
 }
--- Spring-OktaConnect/src/main/resources/client_details.md
+++ Spring-OktaConnect/src/main/resources/client_details.md
@@ -5,3 +5,8 @@
 
 ### Client secret 
 H5FO8AR-5AJSwE_zppfzI2xOnaqILNsQHtmnkitT
+
+### Token for API calls
+key: Demo
+
+value: 003jWgSL3o_Hy_vC5fh4mwT5qr3NNSrzV2r67zcBkf
