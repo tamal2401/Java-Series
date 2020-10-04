4fce74d8a5534cc814b1e7e91020fe4e890f49e2
Tamal2401 <das.tamal00496@gmail.com>
2020-10-04 23:44:57 +0530

    multiple datasource project added

---
+++ Spring-Multipledatasource/src/main/java/com/spring/jpa/SpringMultipledatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultipledatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultipledatasourceApplication.class, args);
+	}
+
+}
--- Spring-Multipledatasource/src/main/java/com/spring/jpa/custom/DsConfiguration.java
+++ Spring-Multipledatasource/src/main/java/com/spring/jpa/custom/DsConfiguration.java
@@ -1,23 +1,71 @@
 package com.spring.jpa.custom;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 
 @Configuration
+@ConfigurationProperties(prefix = "app.custom.datasource")
 public class DsConfiguration {
 
-    @ConfigurationProperties(prefix = "app.custom.datasource")
-    @Bean
-    @Primary // this will override the datasource autoconfiguration and use your own everywhere
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Bean("CustomDatasource")
     public DataSource dataSource() {
         return DataSourceBuilder
                 .create()
+                .url(this.url)
+                .username(this.username)
+                .password(this.password)
                 .build();
+    }
+
+    @Bean("CustomTemplate")
+    public JdbcTemplate getTemplate() {
+        JdbcTemplate template = new JdbcTemplate(dataSource());
+        return template;
     }
 }
