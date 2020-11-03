b9cd37499aed6807bb548983b48a4abe8d64c54a
Tamal2401 <das.tamal00496@gmail.com>
2020-11-03 23:55:44 +0530

    swaggerUi change

--- Spring-Cloud-Services/dashboard/src/main/java/com/demo/dashboard/ViewController.java
+++ Spring-Cloud-Services/dashboard/src/main/java/com/demo/dashboard/ViewController.java
@@ -1,9 +1,14 @@
 package com.demo.dashboard;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ViewController {
 
+    @PostMapping(value = "/api/product/create", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void createProduct(){
 
+    }
 }
