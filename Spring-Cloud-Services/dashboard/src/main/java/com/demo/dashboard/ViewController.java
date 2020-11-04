8f5a2756c36237cf5c34a174d13dd802586dbf98
Tamal2401 <das.tamal00496@gmail.com>
2020-11-04 12:58:50 +0530

    swagger Ui working code added

--- Spring-Cloud-Services/dashboard/src/main/java/com/demo/dashboard/ViewController.java
+++ Spring-Cloud-Services/dashboard/src/main/java/com/demo/dashboard/ViewController.java
@@ -1,14 +1,30 @@
 package com.demo.dashboard;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ViewController {
 
     @PostMapping(value = "/api/product/create", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void createProduct(){
+    public void createProduct(@RequestBody Product prod) {
 
+    }
+
+    @PostMapping(value = "/api/product/update", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void updateProduct(@RequestBody Product prod) {
+
+    }
+
+    @DeleteMapping(value = "/api/product/delete/{id}")
+    public void deleteProduct(@PathVariable Integer id) {
+
+    }
+
+    @GetMapping(value = "/api/product/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Product> getAllProduct() {
+        return null;
     }
 }
