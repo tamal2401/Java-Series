9a8ca16f02b0314bfe20270864ed0d744c1a424d
Tamal2401 <das.tamal00496@gmail.com>
2020-11-15 23:54:00 +0530

    kafka controller completed

--- Spring-Cloud-Services/dequer/src/main/java/com/demo/dequer/DequerApplication.java
+++ Spring-Cloud-Services/dequer/src/main/java/com/demo/dequer/DequerApplication.java
@@ -1,13 +1,42 @@
 package com.demo.dequer;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@EnableKafka
 public class DequerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DequerApplication.class, args);
-	}
+    @Value("${kafka.server.address}")
+    private String bootstrapAddress;
 
+    public static void main(String[] args) {
+        SpringApplication.run(DequerApplication.class, args);
+    }
+
+    public ConsumerFactory<String, String> getConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapAddress);
+        props.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                "test_grpID");
+        props.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
 }
