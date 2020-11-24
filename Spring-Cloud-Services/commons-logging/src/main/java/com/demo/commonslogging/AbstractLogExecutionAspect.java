b22d3e68688ac725984d0626bafe356cb1769e60
Tamal2401 <das.tamal00496@gmail.com>
2020-11-24 22:44:30 +0530

    logexecutionaop aspect class added

--- Spring-Cloud-Services/commons-logging/src/main/java/com/demo/commonslogging/AbstractLogExecutionAspect.java
+++ Spring-Cloud-Services/commons-logging/src/main/java/com/demo/commonslogging/AbstractLogExecutionAspect.java
@@ -1,4 +1,24 @@
 package com.demo.commonslogging;
 
+import com.google.common.base.Stopwatch;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public abstract class AbstractLogExecutionAspect {
+
+    @Around("@annotation(com.demo.commonslogging.LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        Stopwatch timer = Stopwatch.createStarted();
+
+        Object execVal = joinPoint.proceed();
+
+        timer.stop();
+
+
+        return  null;
+    }
 }
