34f8b7b09031ca2dc7e94b265cc7500ffaf8bedf
Tamal2401 <das.tamal00496@gmail.com>
2020-11-26 23:25:24 +0530

    timer logging added via AOP

--- Spring-Cloud-Services/commons-logging/src/main/java/com/demo/commonslogging/AbstractLogExecutionAspect.java
+++ Spring-Cloud-Services/commons-logging/src/main/java/com/demo/commonslogging/AbstractLogExecutionAspect.java
@@ -1,24 +1,49 @@
 package com.demo.commonslogging;
 
 import com.google.common.base.Stopwatch;
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 @Aspect
 @Component
 public abstract class AbstractLogExecutionAspect {
 
+    private static final String CLAZZ = "class";
+    private static final String ACTION = "action";
+    private static final String ELAPSE_TIME = "elapsedTime";
+    private static final String DURATION = "duration";
+
     @Around("@annotation(com.demo.commonslogging.LogExecutionTime)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         Stopwatch timer = Stopwatch.createStarted();
-
         Object execVal = joinPoint.proceed();
-
         timer.stop();
 
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Arrays.stream(signature.getMethod().getAnnotations())
+                .filter(annotation -> annotation instanceof LogExecutionTime)
+                .forEach(annotation -> {
+                    String action = StringUtils.defaultIfBlank(((LogExecutionTime) annotation).action(), joinPoint.getSignature().getName());
+                    String clazz = joinPoint.getSignature().getDeclaringType().getName();
+                    long duration = timer.elapsed(TimeUnit.MILLISECONDS);
 
+                    MDC.put(CLAZZ, clazz);
+                    MDC.put(ACTION, action);
+                    MDC.put(DURATION, String.valueOf(duration));
+                    MDC.put(ELAPSE_TIME, String.valueOf(duration));
+
+
+
+                });
         return  null;
     }
 }
---
+++ Spring-Cloud-Services/commons-logging/src/main/java/com/demo/commonslogging/LoggingUtil.java
@@ -0,0 +1,4 @@
+package com.demo.commonslogging;
+
+public class LoggingUtil {
+}
