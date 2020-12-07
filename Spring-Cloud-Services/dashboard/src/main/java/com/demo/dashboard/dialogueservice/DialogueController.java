b86406a5e5129d44850d2cac96be348286397961
Tamal2401 <das.tamal00496@gmail.com>
2020-12-07 23:35:11 +0530

    Message limit changes

--- Spring-Cloud-Services/dashboard/src/main/java/com/demo/dashboard/dialogueservice/DialogueController.java
+++ Spring-Cloud-Services/dashboard/src/main/java/com/demo/dashboard/dialogueservice/DialogueController.java
@@ -3,21 +3,16 @@ package com.demo.dashboard.dialogueservice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-@RestController
+@Controller
 public class DialogueController {
 
     private final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
