53eb621ec258838b2b134cfd1207150a177a46e6
Tamal2401 <das.tamal00496@gmail.com>
2020-12-14 23:46:38 +0530

    zuul proxy

--- Spring-Cloud-Services/proxy/src/main/java/com/demo/proxy/ProxyServerApplication.java
+++ Spring-Cloud-Services/proxy/src/main/java/com/demo/proxy/ProxyServerApplication.java
@@ -2,8 +2,10 @@ package com.demo.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ProxyServerApplication {
 
 	public static void main(String[] args) {
