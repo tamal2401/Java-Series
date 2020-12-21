cea718941b577685d9541b85101cee82ef04c15f
Tamal2401 <das.tamal00496@gmail.com>
2020-12-21 22:43:48 +0530

    threadpool created

--- Java-Custom-DS/src/main/java/com/java/ds/concurrency/threadpool/CustomThreadPool.java
+++ Java-Custom-DS/src/main/java/com/java/ds/concurrency/threadpool/CustomThreadPool.java
@@ -2,6 +2,7 @@ package com.java.ds.concurrency.threadpool;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.stream.IntStream;
 
 /**
  * Custom Thread Pool implementation. Where all new tasks are queued in a BlockingQueue and once new
@@ -15,15 +16,30 @@ public class CustomThreadPool {
 
 	private final BlockingQueue<Runnable> workerQueue;
 	private Thread[] workerThread;
+	private boolean isStopped = false;
 
 	public CustomThreadPool(int numOfThreads) {
-		workerQueue = new LinkedBlockingDeque<Runnable>();
+		workerQueue = new LinkedBlockingDeque<>();
 		workerThread = new Thread[numOfThreads];
+		int i = 0;
+
+		for(Thread eachThread:workerThread) {
+			eachThread = new Worker("custom-thread-"+i);
+			workerThread[i]=eachThread;
+			i++;
+			eachThread.start();
+		}
 	}
 
-	public static void main(String[] args) {
-		Thread th = new Thread(()->System.out.println("hi"));
-		th.run();
+	public synchronized void execute(Runnable task) {
+		if(!isStopped) this.workerQueue.add(task);
+	}
+
+	public synchronized void shutDown() {
+		this.isStopped = true;
+		for(Thread eachThread:workerThread) {
+			eachThread.interrupt();	
+		}
 	}
 
 	class Worker extends Thread {
@@ -33,11 +49,29 @@ public class CustomThreadPool {
 
 		@Override
 		public void run() {
-			try {
-				workerQueue.take().run();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			while(!isStopped) {
+				try {
+					workerQueue.take().run();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		CustomThreadPool pool = new CustomThreadPool(8);
+		IntStream.range(0, 5).forEach(digit -> {
+			pool.execute(()-> {
+				System.out.println("Current running thread is :: "+Thread.currentThread().getName());
+				//				try {
+				//					Thread.sleep(2000);
+				//				} catch (InterruptedException e) {
+				//					e.printStackTrace();
+				//				}
+			});
+		});
+		pool.shutDown();
+		Thread.sleep(5000);
 	}
 }
