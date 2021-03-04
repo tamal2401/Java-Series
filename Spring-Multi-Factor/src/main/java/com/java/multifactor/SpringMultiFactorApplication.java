630db14cb1ca922f47d7515a9b0f980128156379
Tamal2401 <das.tamal00496@gmail.com>
2021-03-04 23:03:35 +0530

    Entity classes are added

---
+++ Spring-Multi-Factor/src/main/java/com/java/multifactor/SpringMultiFactorApplication.java
@@ -0,0 +1,13 @@
+package com.java.multifactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiFactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultiFactorApplication.class, args);
+	}
+
+}
---
+++ Spring-Multi-Factor/src/test/java/com/java/multifactor/SpringMultiFactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.multifactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiFactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
