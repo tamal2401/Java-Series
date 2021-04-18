3ec24449b256d29a2d533d29b8fca57be12caaa0
Tamal2401 <das.tamal00496@gmail.com>
2021-04-19 00:39:24 +0530

    all services integration is completed

--- Spring-Zuul-Hystrix/Spring-Admin/src/main/java/com/spring/admin/WebSecurituConfiguration.java
+++
@@ -1,4 +0,0 @@
-package com.spring.admin;
-
-public class WebSecurituConfiguration {
-}
--- Spring-Zuul-Hystrix/zuul/src/main/java/com/cloud/zuul/ZuulApplication.java
+++ Spring-Zuul-Hystrix/zuul/src/main/java/com/cloud/zuul/ZuulApplication.java
@@ -2,10 +2,14 @@ package com.cloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
+@ComponentScan("com.cloud.zuul")
 public class ZuulApplication {
 
 	public static void main(String[] args) {
