45db1c9ed2e4daa887d64f496b4b63924c2ceaa6
Tamal2401 <das.tamal00496@gmail.com>
2021-04-22 03:02:32 +0530

    Spring boot admin board and config added

--- Spring-Zuul-Hystrix/Spring-Admin/src/main/java/com/spring/admin/SecurityConfigurerAdapter.java
+++ Spring-Zuul-Hystrix/Spring-Admin/src/main/java/com/spring/admin/SecurityConfigurerAdapter.java
@@ -1,16 +1,77 @@
 package com.spring.admin;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
-@EnableWebSecurity
+import java.util.UUID;
+
+@Configuration(proxyBeanMethods = false)
+@PropertySource("classpath:auth.properties")
 public class SecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
+    @Value("${mesh.security.auth.username}")
+    private String userName;
+    @Value("${mesh.security.auth.password}")
+    private String pwd;
+    @Value("${mesh.security.auth.role}")
+    private String role;
+
+    private final AdminServerProperties adminServer;
+
+    public SecurityConfigurerAdapter(AdminServerProperties adminServer) {
+        this.adminServer = adminServer;
+    }
+
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(this.adminServer.path("/wallboard"));
+
+        http.authorizeRequests(
+                (authorizeRequests) -> authorizeRequests.antMatchers(this.adminServer.path("/assets/**")).permitAll()
+                        .antMatchers(this.adminServer.path("/login")).permitAll().anyRequest().authenticated()
+        ).formLogin(
+                (formLogin) -> formLogin.loginPage(this.adminServer.path("/login")).successHandler(successHandler).and()
+        ).logout((logout) -> logout.logoutUrl(this.adminServer.path("/logout"))).httpBasic(Customizer.withDefaults())
+                .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers(
+                                new AntPathRequestMatcher(this.adminServer.path("/instances"),
+                                        HttpMethod.POST.toString()),
+                                new AntPathRequestMatcher(this.adminServer.path("/instances/*"),
+                                        HttpMethod.DELETE.toString()),
+                                new AntPathRequestMatcher(this.adminServer.path("/actuator/**"))
+                        ))
+                .rememberMe((rememberMe) -> rememberMe.key(UUID.randomUUID().toString()).tokenValiditySeconds(648000));
+    }
+
+    // Required to provide UserDetailsService for "remember functionality"
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        //auth.inMemoryAuthentication().passwordEncoder(encoder).withUser(userName).password(pwd).roles(role);
+        auth.userDetailsService(inMemoryUserDetailsManager()).passwordEncoder(encoder);
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        final User user = new User(userName, pwd, AuthorityUtils.commaSeparatedStringToAuthorityList(role));
+        return new InMemoryUserDetailsManager(user);
     }
 }
--- Spring-Zuul-Hystrix/Spring-Admin/src/main/java/com/spring/admin/SpringAdminApplication.java
+++ Spring-Zuul-Hystrix/Spring-Admin/src/main/java/com/spring/admin/SpringAdminApplication.java
@@ -4,8 +4,10 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@Configuration
 @EnableAdminServer
 @SpringBootApplication
 @EnableDiscoveryClient
