615dcc193a4d55fd81fefb9e1ffe492655960cd4
Tamal2401 <das.tamal00496@gmail.com>
2021-04-23 02:23:42 +0530

    refactoring spring security code

--- Spring-Zuul-Hystrix/book/src/main/java/com/cloud/book/BookApplication.java
+++ Spring-Zuul-Hystrix/book/src/main/java/com/cloud/book/BookApplication.java
@@ -1,11 +1,14 @@
 package com.cloud.book;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAutoConfiguration(exclude = {EurekaClientAutoConfiguration.class})
 public class BookApplication {
 
 	public static void main(String[] args) {
