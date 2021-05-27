3ad080a4d5122d3b2e04a3026044ee150e1a4052
Tamal2401 <das.tamal00496@gmail.com>
2021-05-28 00:09:26 +0530

    bug fixes

--- Spring-Kafka-Streams/Kafka_Order_Generation/src/main/java/com/kafka/demo/order/generation/service/OrderGenerationServiceImpl.java
+++ Spring-Kafka-Streams/Kafka_Order_Generation/src/main/java/com/kafka/demo/order/generation/service/OrderGenerationServiceImpl.java
@@ -1,21 +1,31 @@
 package com.kafka.demo.order.generation.service;
 
 import com.kafka.demo.order.generation.channel.OrderDeliveryChannel;
+import com.kafka.demo.order.generation.dao.OrderArchivalDao;
 import com.kafka.demo.order.generation.entity.OrderReceivedEntity;
+import com.kafka.demo.order.generation.model.Commodity;
 import com.kafka.demo.order.generation.model.OrderModel;
+import com.kafka.demo.order.generation.model.RequestModel;
+import com.kafka.demo.order.generation.model.User;
 import com.kafka.demo.order.generation.producer.OrderGenerationPublisher;
 import com.kafka.demo.order.generation.repository.OrderRepository;
 import com.kafka.demo.order.generation.util.LoggUtil;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class OrderGenerationServiceImpl implements OrderGenerationService {
 
     private static final Logger log = LoggUtil.getLogger(OrderGenerationServiceImpl.class);
 
-    private OrderRepository orderRepository;
+    private OrderArchivalDao orderArchivalDao;
+
+    @Value("${topic.orderGeneration}")
+    String generationTopicName;
 
     @Autowired
     OrderGenerationPublisher orderGenerationPublisher;
@@ -23,31 +33,48 @@ public class OrderGenerationServiceImpl implements OrderGenerationService {
     @Autowired
     OrderDeliveryChannel channel;
 
-    public OrderGenerationServiceImpl(OrderRepository repository) {
-        this.orderRepository = repository;
+    public OrderGenerationServiceImpl(OrderArchivalDao archivalDao) {
+        this.orderArchivalDao = archivalDao;
     }
 
     @Override
-    public void saveAndPublishOrder(OrderModel model) {
-        OrderReceivedEntity entity = new OrderReceivedEntity();
+    public OrderModel saveAndPublishOrder(RequestModel reqModel) {
+        OrderModel model = new OrderModel();
+        buildOrderModel(model, reqModel);
 
-        buildReqEntity(model, entity);
+        getUserInformation(model, reqModel.getUserId());
 
-        OrderReceivedEntity orderEntity = this.orderRepository.save(entity);
-
+        OrderReceivedEntity orderEntity = this.orderArchivalDao.saveOrder(model);
         model.setOrderId(orderEntity.getId());
+
         log.info("Order generation request published with order :: {}", model);
         orderGenerationPublisher.send(model, channel.orderGenerationRequested());
+        return model;
     }
 
-    private void buildReqEntity(OrderModel model, OrderReceivedEntity entity) {
-        entity.setEventId(model.getEventId());
-        entity.setPaymentMode(model.getPaymentMode());
-        entity.setProcessedDateTime(model.getProcessedDateTime());
-        entity.setTransactionId(model.getTransactionId());
-        entity.setCategoryId(model.getCommodity().getCategoryId());
-        entity.setCategoryName(model.getCommodity().getCategoryName());
-        entity.setQuantity(model.getCommodity().getQuantity());
-        entity.setProductName(model.getCommodity().getProductName());
+    private void getUserInformation(OrderModel model, String userId) {
+        User currUser = new User();
+        currUser.setUserId(userId);
+        // Setting mock values. In Prod one can hit user details DB to get the proper information.
+        currUser.setUserName("tamalD");
+        currUser.setMail("onlyjohn98@gmail.com");
+
+        model.setUser(currUser);
+    }
+
+
+    private void buildOrderModel(OrderModel model, RequestModel reqModel) {
+        Commodity comm = new Commodity();
+        comm.setCategoryId(reqModel.getCategoryId());
+        comm.setCategoryName(reqModel.getCategoryName());
+        comm.setQuantity(reqModel.getQuantity());
+        comm.setProductName(reqModel.getProductName());
+
+        model.setEventId(UUID.randomUUID().toString());
+        model.setPaymentMode(reqModel.getPaymentMode());
+        model.setEventType(generationTopicName);
+        model.setProcessedDateTime(reqModel.getProcessedDateTime());
+        model.setTransactionId(reqModel.getTransactionId());
+        model.setCommodity(comm);
     }
 }
