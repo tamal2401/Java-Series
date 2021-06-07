a0c19edefb37cdff5dcff816619df1f0eaeb7844
Tamal2401 <das.tamal00496@gmail.com>
2021-06-08 03:08:05 +0530

    code changes

---
+++ Spring-Kafka-Streams/Kafka_Order_Inventory/src/main/java/com/kafka/demo/order/inventory/model/Commodity.java
@@ -0,0 +1,20 @@
+package com.kafka.demo.order.inventory.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@ToString
+public class Commodity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String categoryName;
+    private long categoryId;
+    private int quantity;
+    private String productName;
+}
