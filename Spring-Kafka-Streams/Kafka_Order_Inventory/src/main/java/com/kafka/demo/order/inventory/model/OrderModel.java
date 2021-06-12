fea1589e7d535d50b715b61476ada634e4876e2e
Tamal2401 <das.tamal00496@gmail.com>
2021-06-12 22:44:18 +0530

    code changes

---
+++ Spring-Kafka-Streams/Kafka_Order_Inventory/src/main/java/com/kafka/demo/order/inventory/model/OrderModel.java
@@ -0,0 +1,27 @@
+package com.kafka.demo.order.inventory.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.validation.Valid;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@ToString
+public class OrderModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private long orderId;
+    private String eventId;
+    private String eventType;
+    private String processedDateTime;
+    private String paymentMode;
+    private String transactionId;
+    @Valid
+    private Commodity commodity;
+    @Valid
+    private User user;
+}
