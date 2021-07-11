0410809f01fe76f8e5404e8f49847c6871ff008d
Tamal2401 <das.tamal00496@gmail.com>
2021-07-11 23:08:37 +0530

    Create Java.java

---
+++ Java-Custom-DS/src/main/java/com/java/ds/hackerrank/Java.java
@@ -0,0 +1,14 @@
+package com.java.ds.hackerrank;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.*;
+
+public class Java {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("Tamal", "Das");
+        map.put("Pallavi", "Dutta");
+        System.out.println(map);
+    }
+}
