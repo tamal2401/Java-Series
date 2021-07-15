f513af655e513988f19c101ed7a703ebd9150b47
Tamal2401 <das.tamal00496@gmail.com>
2021-07-15 22:42:04 +0530

    Update SecurityConfigurerAdapter.java

--- Spring-Boot-Swagger/book/src/main/java/com/cloud/book/SecurityConfigurerAdapter.java
+++ Spring-Boot-Swagger/book/src/main/java/com/cloud/book/SecurityConfigurerAdapter.java
@@ -1,12 +1,18 @@
 package com.cloud.book;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -15,6 +21,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @PropertySource("classpath:auth.properties")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
     @Value("${mesh.security.auth.username}")
@@ -23,16 +30,32 @@ public class SecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
     private String pwd;
     @Value("${mesh.security.auth.role}")
     private String role;
-    @Value("${mesh.security.auth.client.pwd}")
-    private String clientPwd;
+    @Value("${security.swagger.enabled: false}")
+    public boolean swaggerFlag;
+    @Autowired
+    CustomBasicAuthEntryPoint customBasicAuthEntryPoint;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        if (swaggerFlag) {
+            http.authorizeRequests().antMatchers("/v2/api-docs",
+                    "/configuration/ui",
+                    "/swagger-resources/**",
+                    "/configuration/security",
+                    "/swagger-ui.html",
+                    "/swagger-ui/",
+                    "/webjars/**").permitAll();
+        }
+
+        HttpBasicConfigurer<HttpSecurity> configurer = http.csrf().disable()
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+
+        if(swaggerFlag){
+            configurer.authenticationEntryPoint(customBasicAuthEntryPoint);
+        }
     }
 
     @Override
@@ -43,7 +66,10 @@ public class SecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        final User user = new User(this.userName, this.pwd, AuthorityUtils.commaSeparatedStringToAuthorityList(this.role));
+        final User user = new User(
+                this.userName,
+                this.pwd,
+                AuthorityUtils.commaSeparatedStringToAuthorityList(this.role));
         return new InMemoryUserDetailsManager(user);
     }
 }
