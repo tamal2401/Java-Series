7c2721af5a86cf44a9146a83b7d6d4c3d5a81730
Tamal2401 <das.tamal00496@gmail.com>
2021-07-16 23:00:45 +0530

    Create ZipkinServiceApplicationTests.java

---
+++ Spring-Zipkin/zipkin/src/test/java/com/demo/zipkin/ZipkinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
