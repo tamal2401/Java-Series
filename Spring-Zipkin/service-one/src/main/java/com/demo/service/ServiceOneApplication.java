73dc7f8a93f29e8e71f5330c90c8a5917b6dc384
Tamal2401 <das.tamal00496@gmail.com>
2021-07-27 23:13:00 +0530

    Create ServiceOneApplication.java

---
+++ Spring-Zipkin/service-one/src/main/java/com/demo/service/ServiceOneApplication.java
@@ -0,0 +1,19 @@
+package com.demo.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ServiceOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOneApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate template(){
+		return new RestTemplate();
+	}
+}
