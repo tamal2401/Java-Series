57ddb9758c9e210fc3d484b70e7fec5dbdb7beb0
Tamal2401 <das.tamal00496@gmail.com>
2021-07-30 22:41:07 +0530

    Update MainController.java

--- Spring-Zipkin/service-two/src/main/java/com/demo/service/controller/MainController.java
+++ Spring-Zipkin/service-two/src/main/java/com/demo/service/controller/MainController.java
@@ -4,7 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.*;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class MainController {
@@ -13,14 +17,15 @@ public class MainController {
     public MainController(RestTemplate template) {
         this.template = template;
     }
+
     @GetMapping("/v1/two")
     public String process() throws Exception {
         String response = this.template.getForObject("http://localhost:8084/v1/three", String.class);
         int val = ThreadLocalRandom.current().nextInt(1, 100);
-        if(val%2!=0){
+        if (val % 2 != 0) {
             throw new Exception("Boom!!!! odd things happened in life");
         }
         Thread.sleep(3000);
-        return "Service 2 <- "+response;
+        return "Service 2 <- " + response;
     }
 }
