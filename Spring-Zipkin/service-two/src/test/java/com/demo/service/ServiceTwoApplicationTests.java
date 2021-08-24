541773fbb467ba736b8c7a6a11155004e208d65d
Tamal2401 <das.tamal00496@gmail.com>
2021-08-24 22:49:15 +0530

    Create ServiceTwoApplicationTests.java

---
+++ Spring-Zipkin/service-two/src/test/java/com/demo/service/ServiceTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
