4131464be1e43484904ca60b86a540ceeda304af
Tamal2401 <das.tamal00496@gmail.com>
2022-02-14 23:29:54 +0530

    commit

---
+++ Spring-Task-Scheduler/src/main/java/com/demo/schedule/exception/CommonException.java
@@ -0,0 +1,16 @@
+package com.demo.schedule.exception;
+
+public class CommonException extends Exception{
+
+    public CommonException() {
+        super();
+    }
+
+    public CommonException(String message) {
+        super(message);
+    }
+
+    public CommonException(Throwable cause) {
+        super(cause);
+    }
+}
