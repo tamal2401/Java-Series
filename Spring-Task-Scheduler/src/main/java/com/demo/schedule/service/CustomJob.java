951e765f30613bfa8b962d56c9bb16a4704309ea
Tamal2401 <das.tamal00496@gmail.com>
2022-02-17 02:19:27 +0530

    commit

---
+++ Spring-Task-Scheduler/src/main/java/com/demo/schedule/service/CustomJob.java
@@ -0,0 +1,17 @@
+package com.demo.schedule.service;
+
+import com.demo.schedule.job.ScheduleJob;
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+
+public class CustomJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) {
+        final JobDataMap dataMap = jobExecutionContext.getMergedJobDataMap();
+        ScheduleJob job = (ScheduleJob) dataMap.get("scheduleJob");
+        TaskService service = (TaskService) dataMap.get("taskExecutor");
+        service.executeTask(job);
+    }
+}
