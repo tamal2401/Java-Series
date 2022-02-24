7d9834f5c2e86897d7ee6690b97a6987baebc891
Tamal2401 <das.tamal00496@gmail.com>
2022-02-24 23:30:38 +0530

    commit

---
+++ Spring-Task-Scheduler/src/main/java/com/demo/schedule/logger/LoggerEntry.java
@@ -0,0 +1,12 @@
+package com.demo.schedule.logger;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoggerEntry {
+
+}
