d7fdbca2741aee4e73e1d86e720f5299782fc631
Tamal Das <das.tamal00496@gmail.com>
2022-04-02 00:35:57 +0530

    comparator added

---
+++ Spring-Task-Scheduler/src/main/java/com/demo/schedule/TaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSchedulerApplication.class, args);
+	}
+
+}
---
+++ Spring-Task-Scheduler/src/test/java/com/demo/schedule/TaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
